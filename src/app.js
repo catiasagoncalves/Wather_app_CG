c68881ef18f30c1bcb79c7ad98f084247acb20f0
catiasagoncalves <catiasagoncalves@gmail.com>
2021-08-29 14:33:06 +0200

    added temperature discription in JS

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-let now=new Date();
-let hours=now.getHours();
-let minutes=now.getMinutes();
-let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day=days[now.getDay()];
-let h1=document.querySelector ("li");
-h1.innerHTML=`${day}, ${hours}:${minutes}`;
-
-function showLocation(event) {
-  event.preventDefault();
-  let apiKey = "0bca7a6e963a4888aee2f2257270c526";
-let city = document.querySelector("#location-input").value;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+function displayTemperature (response){
+  console.log (response.data);
+  let temperatureElement=document.querySelector("#temperature");
+  temperatureElement.innerHTML=Math.round(response.data.main.temp);
+  let cityElement=document.querySelector("#city");
+  cityElement.innerHTML=response.data.name;
+  let descriptionElement=document.querySelector("#weather-description");
+  descriptionElement.innerHTML=response.data.weather[0].description;
+  let humidityElement=document.querySelector("#humidity");
+  humidityElement.innerHTML=response.data.main.humidity;
+  let windElement=document.querySelector("#wind");
+  windElement.innerHTML=Math.round(response.data.wind.speed);
+   let feelsLikeElement=document.querySelector("#feels-like");
+  feelsLikeElement.innerHTML=Math.round(response.data.main.feels_like);
 }
+
+
+  let apiKey = "0bca7a6e963a4888aee2f2257270c526";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+
