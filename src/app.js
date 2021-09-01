b8bfc55d4ed088cd07b1ebe11170438da8c6a9fa
catiasagoncalves <catiasagoncalves@gmail.com>
2021-09-01 22:01:46 +0200

    include the new API for the daily forecast

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function actualtDate(timestamp) {
   return `${day} at ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
+  console.log(response.data.daily);
   let forecastElement=document.querySelector("#forecast");
   let forecastHTML=`<div class="row">`;
 
@@ -48,6 +49,14 @@ forecastElement.innerHTML=forecastHTML;
 }
 
 
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey="0bca7a6e963a4888aee2f2257270c526";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+
+}
+
 function displayTemperature (response){
    let temperatureElement=document.querySelector("#temperature");
   let cityElement=document.querySelector("#city");
@@ -72,7 +81,12 @@ iconElement.setAttribute(    "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast (response.data.coord);
 }
+
+
+
+
  function searchCity (city){
 let apiKey = "0bca7a6e963a4888aee2f2257270c526";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -116,4 +130,4 @@ let celsiusLink=document.querySelector("#celsius-temp");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 searchCity("Luxembourg");
-displayForecast();
+
