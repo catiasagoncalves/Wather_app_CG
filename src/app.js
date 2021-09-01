ed53b206fa2ca8af22fe4a1dc7560c10385217d7
catiasagoncalves <catiasagoncalves@gmail.com>
2021-09-01 22:31:56 +0200

    Display the forescast data, using the new API

--- src/app.js
+++ src/app.js
@@ -21,26 +21,36 @@ function actualtDate(timestamp) {
   return `${day} at ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+  let date=new Date(timestamp * 1000);
+  let day= date.getDay();
+  let days= ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days [day];
+
+}
+
 function displayForecast(response){
-  console.log(response.data.daily);
+  let forecast =response.data.daily;
+
   let forecastElement=document.querySelector("#forecast");
   let forecastHTML=`<div class="row">`;
 
-  let days= ["Thu" , "Fri", "Sat", "Sun"];
-  days.forEach(function(day) {
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 6){
 forecastHTML=forecastHTML + `
   <div class="col-2">
     <div class="forecast-week-day">
-         ${day}  
+         ${formatDay(forecastDay.dt)}  
     </div>
-     <img src="#" alt="">
+     <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42">
      <div class="forecast-temperature">
-    <span class="forecast-max-temp">
-        18ºC
+    <span class="forecast-max-temp">${Math.round(forecastDay.temp.max)}
+        ºC
     </span>
-    <span class="forecast-min-temp">12ºC</span> 
+    <span class="forecast-min-temp"> ${Math.round(forecastDay.temp.min)}ºC</span> 
          </div>
-  </div>`;
+  </div>`;}
 
   }  );
   
